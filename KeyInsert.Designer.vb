d30fec59e6804a996c3f8bb9c8e4fea39d4cc97f
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2015-11-28 21:48:14 +0200

    Added first code, doing other stuff so committing what I have so far

--- KeyInsert.Designer.vb
+++ KeyInsert.Designer.vb
@@ -31,15 +31,111 @@ Partial Class KeyInsert
     ''' not be able to load this method if it was changed manually.
     ''' </summary>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lstKeyStrokes = New System.Windows.Forms.ListView()
+        Me.colheadKeyStroke = New System.Windows.Forms.ColumnHeader()
+        Me.colheadTime = New System.Windows.Forms.ColumnHeader()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnHotkey = New System.Windows.Forms.Button()
+        Me.timerKeyChecker = New System.Windows.Forms.Timer(Me.components)
+        Me.lblStatus = New System.Windows.Forms.Label()
         Me.SuspendLayout
+        '
+        'lstKeyStrokes
+        '
+        Me.lstKeyStrokes.AllowDrop = true
+        Me.lstKeyStrokes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom)  _
+                        Or System.Windows.Forms.AnchorStyles.Left)  _
+                        Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.lstKeyStrokes.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colheadKeyStroke, Me.colheadTime})
+        Me.lstKeyStrokes.FullRowSelect = true
+        Me.lstKeyStrokes.GridLines = true
+        Me.lstKeyStrokes.HideSelection = false
+        Me.lstKeyStrokes.LabelEdit = true
+        Me.lstKeyStrokes.Location = New System.Drawing.Point(12, 12)
+        Me.lstKeyStrokes.MultiSelect = false
+        Me.lstKeyStrokes.Name = "lstKeyStrokes"
+        Me.lstKeyStrokes.Size = New System.Drawing.Size(608, 204)
+        Me.lstKeyStrokes.TabIndex = 22
+        Me.lstKeyStrokes.UseCompatibleStateImageBehavior = false
+        Me.lstKeyStrokes.View = System.Windows.Forms.View.Details
+        '
+        'colheadKeyStroke
+        '
+        Me.colheadKeyStroke.Text = "Keystroke"
+        Me.colheadKeyStroke.Width = 469
+        '
+        'colheadTime
+        '
+        Me.colheadTime.Text = "Wait Time"
+        Me.colheadTime.Width = 110
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Location = New System.Drawing.Point(626, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(114, 23)
+        Me.btnAdd.TabIndex = 23
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = true
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnRemove.Location = New System.Drawing.Point(626, 41)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(114, 23)
+        Me.btnRemove.TabIndex = 24
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = true
+        '
+        'btnHotkey
+        '
+        Me.btnHotkey.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.btnHotkey.Location = New System.Drawing.Point(626, 70)
+        Me.btnHotkey.Name = "btnHotkey"
+        Me.btnHotkey.Size = New System.Drawing.Size(114, 23)
+        Me.btnHotkey.TabIndex = 36
+        Me.btnHotkey.Text = "Enable Hotkey (Ctrl)"
+        Me.btnHotkey.UseVisualStyleBackColor = true
+        AddHandler Me.btnHotkey.Click, AddressOf Me.BtnHotkey_Click
+        '
+        'timerKeyChecker
+        '
+        AddHandler Me.timerKeyChecker.Tick, AddressOf Me.TimerKeyChecker_Tick
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = true
+        Me.lblStatus.Location = New System.Drawing.Point(626, 96)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(67, 13)
+        Me.lblStatus.TabIndex = 37
+        Me.lblStatus.Text = "Not Running"
         '
         'KeyInsert
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(752, 228)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnHotkey)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstKeyStrokes)
         Me.Name = "KeyInsert"
         Me.Text = "KeyInsert"
         Me.ResumeLayout(false)
+        Me.PerformLayout
     End Sub
+    Private lblStatus As System.Windows.Forms.Label
+    Private WithEvents timerKeyChecker As System.Windows.Forms.Timer
+    Private WithEvents btnHotkey As System.Windows.Forms.Button
+    Private colheadTime As System.Windows.Forms.ColumnHeader
+    Private WithEvents btnRemove As System.Windows.Forms.Button
+    Private WithEvents btnAdd As System.Windows.Forms.Button
+    Private colheadKeyStroke As System.Windows.Forms.ColumnHeader
+    Private WithEvents lstKeyStrokes As System.Windows.Forms.ListView
 End Class
--- KeyInsert.vb
+++ KeyInsert.vb
@@ -1,16 +1,118 @@
-﻿'
-' Created by SharpDevelop.
-' User: Walkman
-' Date: 2015/11/28
-' Time: 04:26 PM
-'
-Public Partial Class KeyInsert
+﻿Public Partial Class KeyInsert
     Public Sub New()
-        ' The Me.InitializeComponent call is required for Windows Forms designer support.
         Me.InitializeComponent()
-        
-        '
-        ' TODO : Add constructor code after InitializeComponents
-        '
+    End Sub
+    
+    Sub CheckButtons() Handles lstKeyStrokes.Click, lstKeyStrokes.SelectedIndexChanged, lstKeyStrokes.AfterLabelEdit, lstKeyStrokes.ColumnReordered
+        If IsNothing(lstKeyStrokes.FocusedItem) Then
+            btnRemove.Enabled = False
+        Else
+            btnRemove.Enabled = True
+        End If
+    End Sub
+    
+    Sub btnAdd_Click() Handles btnAdd.Click
+        Dim tmpListViewItem As New ListViewItem(New String() {"{ENTER}", "1"})
+        lstKeyStrokes.FocusedItem = lstKeyStrokes.Items.Add(tmpListViewItem)
+        CheckButtons
+    End Sub
+    
+    Sub btnRemove_Click() Handles btnAdd.Click
+        If lstKeyStrokes.SelectedItems.Count > 1 Then
+            For Each item As ListViewItem In lstKeyStrokes.SelectedItems
+                item.Remove
+            Next
+        Else
+            lstKeyStrokes.FocusedItem.Remove
+        End If
+        CheckButtons
+    End Sub
+    
+    Sub btnHotkey_Click() Handles btnHotkey.Click
+        If btnHotkey.Text = "Enable Hotkey (Ctrl)" Then
+            btnHotkey.Text = "Hotkey Enabled!"
+            timerKeyChecker.Interval = 1000
+            timerKeyChecker.Start()
+        ElseIf btnHotkey.Text = "Disable Hotkey (Ctrl)" Then
+            btnHotkey.Text = "Hotkey Disabled!"
+            timerKeyChecker.Interval = 1000
+        End If
+    End Sub
+    
+    Private Sub timerKeyChecker_Tick() Handles timerKeyChecker.Tick
+        If btnHotkey.Text = "Hotkey Enabled!" Then
+            btnHotkey.Text = "Disable Hotkey (Ctrl)"
+            timerKeyChecker.Interval = 100
+        ElseIf btnHotkey.Text = "Hotkey Disabled!" Then
+            btnHotkey.Text = "Enable Hotkey (Ctrl)"
+            timerKeyChecker.Stop()
+        End If
+        If My.Computer.Keyboard.CtrlKeyDown = True Then
+            
+        End If
+    End Sub
+    
+    Sub EditSelectedEntry() Handles lstKeyStrokes.DoubleClick
+        Dim inputBoxText As String
+        If lstKeyStrokes.SelectedItems.Count > 1 Then
+            For Each item As ListViewItem In lstKeyStrokes.SelectedItems
+                inputBoxText = InputBox("Enter Keystroke:", "", item.SubItems.Item(0).Text)
+                If inputBoxText <> "" Then item.SubItems.Item(0).Text = inputBoxText
+                inputBoxText = InputBox("Enter the time to wait after """ & item.Text & """ has been inserted:", "", item.SubItems.Item(1).Text)
+                If inputBoxText <> "" Then item.SubItems.Item(1).Text = inputBoxText
+            Next
+        Else
+            inputBoxText = InputBox("Enter Keystroke:", "", lstKeyStrokes.FocusedItem.SubItems.Item(0).Text)
+            If inputBoxText <> "" Then lstKeyStrokes.FocusedItem.SubItems.Item(0).Text = inputBoxText
+            inputBoxText = InputBox("Enter the time to wait after """ & lstKeyStrokes.FocusedItem.Text & """ has been inserted:", "", lstKeyStrokes.FocusedItem.SubItems.Item(1).Text)
+            If inputBoxText <> "" Then lstKeyStrokes.FocusedItem.SubItems.Item(1).Text = inputBoxText
+        End If
+    End Sub
+    
+    Sub lstKeyStrokes_ColumnClick(sender As Object, e As ColumnClickEventArgs) Handles lstKeyStrokes.ColumnClick
+        lstKeyStrokes.Sorting = IIf(lstKeyStrokes.Sorting = SortOrder.Ascending, SortOrder.Descending, SortOrder.Ascending)
+        lstKeyStrokes.Sort
+    End Sub
+    
+'    Sub ResizeByHeader(sender As Object, e As EventArgs) Handles contextCommandsResizePathHeader.Click, _
+'            contextCommandsResizeArgsHeader.Click, contextCommandsResizeArgHeader.Click
+'        lstKeyStrokes.AutoResizeColumn(sender.Tag, ColumnHeaderAutoResizeStyle.HeaderSize)
+'    End Sub
+'    
+'    Sub ResizeByContent(sender As Object, e As EventArgs) Handles contextCommandsResizePathContent.Click, _
+'            contextCommandsResizeArgsContent.Click, contextCommandsResizeArgContent.Click
+'        lstKeyStrokes.AutoResizeColumn(sender.Tag, ColumnHeaderAutoResizeStyle.ColumnContent)
+'    End Sub
+'    
+'    Sub ResizeAllByHeader() Handles contextCommandsResizeAllHeader.Click
+'        lstKeyStrokes.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+'    End Sub
+'    
+'    Sub ResizeAllByContent() Handles contextCommandsResizeAllContent.Click
+'        lstKeyStrokes.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
+'    End Sub
+    
+    Sub lstKeyStrokes_DragEnter(sender As Object, e As DragEventArgs) Handles lstKeyStrokes.DragEnter
+        If e.Data.GetDataPresent(DataFormats.Text) Or e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.All
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+    
+    Sub lstKeyStrokes_DragDrop(sender As Object, e As DragEventArgs) Handles lstKeyStrokes.DragDrop
+        If e.Data.GetDataPresent(DataFormats.Text) Then
+            Dim tmpListViewItem As New ListViewItem(New String() {e.Data.GetData(DataFormats.Text).ToString, " ", "draggedFile"})
+            lstKeyStrokes.FocusedItem = lstKeyStrokes.Items.Add(tmpListViewItem)
+        ElseIf e.Data.GetDataPresent(DataFormats.FileDrop)
+            For i = 0 To Integer.MaxValue
+                If (e.Data.GetData(DataFormats.FileDrop)(i) <> Nothing) Then
+                    Dim tmpListViewItem As New ListViewItem(New String() {e.Data.GetData(DataFormats.FileDrop)(i), " ", "draggedFile"})
+                    lstKeyStrokes.FocusedItem = lstKeyStrokes.Items.Add(tmpListViewItem)
+                Else
+                    Exit For
+                End If
+            Next
+        End If
     End Sub
 End Class
